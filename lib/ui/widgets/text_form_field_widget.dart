4f7035555fc30f86331b8035ff0d1b4c752a43bd
thevoidloop <marroquin_daniel@outlook.com>
2023-05-03 12:05:40 -0600

    formateo de codigo

--- lib/ui/widgets/text_form_field_widget.dart
+++ lib/ui/widgets/text_form_field_widget.dart
@@ -4,41 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class TextFormFieldNumber extends StatelessWidget {
-  
   final TextEditingController controller;
   final String mensaje;
-  
+
   const TextFormFieldNumber(
-    {
-    super.key,
-    required this.mensaje, 
-    required this.controller
-  });
+      {super.key, required this.mensaje, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    
     return TextFormField(
       controller: controller,
-      decoration:  InputDecoration(
-        labelText: mensaje
-        
-      ),
+      decoration: InputDecoration(labelText: mensaje),
       keyboardType: TextInputType.number,
     );
   }
 }
 
 class TextFormFieldEmail extends StatelessWidget {
-  
   final TextEditingController controller;
   final String mensaje;
-  
+
   const TextFormFieldEmail({
     super.key,
-    required this.controller, required this.mensaje,
+    required this.controller,
+    required this.mensaje,
   });
-
 
   @override
   Widget build(BuildContext context) {
@@ -46,67 +36,58 @@ class TextFormFieldEmail extends StatelessWidget {
       controller: controller,
       decoration: InputDecoration(labelText: mensaje),
       keyboardType: TextInputType.emailAddress,
-      validator: validateEmail
+      validator: validateEmail,
     );
   }
 }
 
 class TextFormFieldPass extends StatelessWidget {
-  
-final TextEditingController controller;
-final String mensaje;
-final bool validator;  
+  final TextEditingController controller;
+  final String mensaje;
+  final bool validator;
 
   const TextFormFieldPass({
-    super.key, required this.controller, 
-    required this.mensaje, 
+    super.key,
+    required this.controller,
+    required this.mensaje,
     this.validator = false,
   });
 
-
   @override
   Widget build(BuildContext context) {
-    
-    
     return ChangeNotifierProvider(
-      create: (context) => UIProvider(),
-      child: Consumer<UIProvider>(
-        builder: (context, ui, child) {
-          return TextFormField(
-            controller: controller,
-            decoration: InputDecoration(
-              labelText: mensaje,
-              suffixIcon: IconButton(
-                icon: ui.obscureText ? const Icon(Icons.visibility_sharp) : const Icon(Icons.visibility_off_sharp),
-                onPressed: ui.toggleObscureText,
-            ),
-            ),
-            obscureText: ui.obscureText,
-            validator: validator ? null : validatePassword
-          );
-        },
-      )
-    );
+        create: (context) => UIProvider(),
+        child: Consumer<UIProvider>(
+          builder: (context, ui, child) {
+            return TextFormField(
+                controller: controller,
+                decoration: InputDecoration(
+                  labelText: mensaje,
+                  suffixIcon: IconButton(
+                    icon: ui.obscureText
+                        ? const Icon(Icons.visibility_sharp)
+                        : const Icon(Icons.visibility_off_sharp),
+                    onPressed: ui.toggleObscureText,
+                  ),
+                ),
+                obscureText: ui.obscureText,
+                validator: validator ? null : validatePassword);
+          },
+        ));
   }
 }
-    
+
 class TextFormFieldText extends StatelessWidget {
-  
   final TextEditingController controller;
   final String mensaje;
 
-  const TextFormFieldText({
-    super.key,
-    required this.mensaje, 
-    required this.controller
-  });
+  const TextFormFieldText(
+      {super.key, required this.mensaje, required this.controller});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      decoration: InputDecoration(
-        labelText: mensaje
-      ),
+      decoration: InputDecoration(labelText: mensaje),
       keyboardType: TextInputType.text,
     );
   }
@@ -133,7 +114,8 @@ class TextFormFielDate extends StatelessWidget {
               ),
             ),
             onTap: () {
-              FocusScope.of(context).requestFocus(FocusNode()); // Ocultar el teclado
+              FocusScope.of(context)
+                  .requestFocus(FocusNode()); // Ocultar el teclado
               _selectDate(context, ui);
             },
             readOnly: true, // Evita que el teclado aparezca al tocar el campo
@@ -142,9 +124,8 @@ class TextFormFielDate extends StatelessWidget {
       ),
     );
   }
-    
 
-    Future<void> _selectDate(BuildContext context, UIProvider ui) async {
+  Future<void> _selectDate(BuildContext context, UIProvider ui) async {
     final DateTime? picked = await showDatePicker(
         context: context,
         initialDate: ui.selectedDate,
@@ -156,6 +137,4 @@ class TextFormFielDate extends StatelessWidget {
       ui.setSelectedDate(picked);
     }
   }
-
-
 }
