12da872825247e8b9e864f06cc5c423258230ce3
thevoidloop <marroquin_daniel@outlook.com>
2023-05-24 14:11:04 -0600

    services_provider borrado

--- lib/data/providers/services_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:conexion/data/models/models.dart';
-import 'package:flutter/material.dart';
-
-class ServicesProviders extends ChangeNotifier {
-  String _valueVehicle = "Seleccione una opcion";
-  late List<Vehicle> _vehicle;
-}
